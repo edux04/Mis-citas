e37d0207b1366b0d6882d1d649d00caaa5490d1d
Eduardo <eduardoramirez0104@gmail.com>
2022-06-18 06:33:28 -0400

    Models and DB context

---
+++ Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mis_citas.Models
+{
+    public partial class Client
+    {
+        public long ClientId { get; set; }
+        public string Name { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string? Cel { get; set; }
+    }
+}
---
+++ Models/citasContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Mis_citas.Models
+{
+    public partial class citasContext : DbContext
+    {
+        public citasContext()
+        {
+        }
+
+        public citasContext(DbContextOptions<citasContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Client> Clients { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=SANCA; Database=citas;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>(entity =>
+            {
+                entity.ToTable("clients");
+
+                entity.Property(e => e.ClientId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("client_id");
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(100)
+                    .HasColumnName("address")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Cel)
+                    .HasMaxLength(100)
+                    .HasColumnName("cel")
+                    .IsFixedLength();
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(100)
+                    .HasColumnName("last_name")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(100)
+                    .HasColumnName("name")
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
